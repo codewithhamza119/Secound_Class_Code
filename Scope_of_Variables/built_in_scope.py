618fcaaa600b5420ab079b8e922ec8fb0765482e
codewithhamza119 <codewithhamza119@gmail.com>
2025-04-04 17:42:56 +0500

    Update built_in_scope.py

--- Scope_of_Variables/built_in_scope.py
+++ Scope_of_Variables/built_in_scope.py
@@ -1,4 +1,4 @@
 #Built-in Scope (Python ke Predefined Functions): Python me jo built-in functions hote hain (jaise print(), len(), max()), wo built-in scope ka hissa hote hain. Inko tum kahin bhi access kar sakti ho.
 
-print(len("Hello"))  # ✅ len() built-in function hai
+print(len("Hello"))  # len() built-in function hai
 # Ye sabse broad scope hota hai, jo hamesha available rehta hai.
